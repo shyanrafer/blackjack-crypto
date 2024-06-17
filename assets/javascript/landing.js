c5ec1793e495aa1ce1561b4a9ba27dfb6a8e4460
The Goose <ryanshaffer1324@gmail.com>
2024-06-17 09:57:22 -0400

    Added screenshots to readme, adjusted project story, added comments to landing page html, css and JS.

--- assets/javascript/landing.js
+++ assets/javascript/landing.js
@@ -1,8 +1,11 @@
+// simple function that removes or adds class - starts modal out at invisible
 document.getElementById('info-button').addEventListener('click', function () {
+  // assigned variable within fx for best practice
   const infoForm = document.getElementById('form-div')
   if (infoForm.classList.contains('invisible')) {
     infoForm.classList.remove('invisible');
     infoForm.classList.add('visible');
+    // also, the text of the button to click changes whether modal is visible or invisible
     this.textContent = 'Hide form';  
   } else {
     infoForm.classList.remove('visbile')
